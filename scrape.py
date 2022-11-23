ff41272a32fd83f245abd3a8b75b24a64a03bc5b
Jekabs <jekabs@gritans.lv>
2022-11-23 17:48:37 +0200

    upd

--- scrape.py
+++ scrape.py
@@ -16,13 +16,28 @@ def get_week_data_by_id(deviceId, week):
     data = json.loads(r.text)['data']
     df = pd.DataFrame(data)
     df['timestamp'] = pd.to_datetime(df['timestamp'])
-    df.set_index('timestamp', inplace=True)
     return df
 
-def get_data_by_id(deviceId):
-    df = pd.DataFrame(columns=['timestamp', 'co2', 'temperature', 'humidity']).set_index('timestamp')
-    for week in tqdm(range(1, weeks)):
+def get_data_by_id(deviceId, start_week=1, end_week=weeks):
+    df = pd.DataFrame(columns=['timestamp', 'co2', 'temperature', 'humidity'])
+    for week in tqdm(range(start_week, end_week)):
         data = get_week_data_by_id(deviceId, week)
         df = pd.concat([df, data])
         sleep(1)
     return df
+
+def scrape_building(building_id, room_ids, start_week=1, end_week=weeks):
+    df = pd.DataFrame(columns=['timestamp', 'room_id', 'co2', 'temperature', 'humidity'])
+    for room_id in (t := tqdm(room_ids)):
+        data = get_data_by_id(room_id, start_week=start_week, end_week=end_week)
+        data['room_id'] = room_id
+        df = pd.concat([df, data])
+        t.set_description("Rooms")
+    return df
+
+if __name__ == "__main__":
+    building_id = 1003
+    rooms_df = pd.read_csv('data/rooms.csv')
+    room_ids = rooms_df[rooms_df['building_id'] == building_id]['id'].values.tolist()
+    df = scrape_building(building_id, room_ids)
+    df.to_csv(f'data/{building_id}.csv', index=False)
